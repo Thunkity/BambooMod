2b13e8f363aea52ada120239d275c398138b8185
Mayochup-Man <91295507+Mayochup-Man@users.noreply.github.com>
2022-12-18 14:36:42 -0600

    Initial commit

---
+++ src/main/java/net/thunkity/bamboo/BambooMod.java
@@ -0,0 +1,14 @@
+package net.thunkity.bamboo;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BambooMod implements ModInitializer {
+	public static final String MOD_ID = "bamboo";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/thunkity/bamboo/BambooModClient.java
@@ -0,0 +1,10 @@
+package net.thunkity.bamboo;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BambooModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
